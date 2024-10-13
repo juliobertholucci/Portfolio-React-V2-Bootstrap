ebc810f3db351a05663b6e4b10c851136484487c
Julio B <juliobertholucciferraz@gmail.com>
2024-10-13 10:46:55 -0300

    adiciona reset CSS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
