380a121e813adb68c400e5dafa718597891140e1
Julio B <juliobertholucciferraz@gmail.com>
2024-10-30 17:29:58 -0300

    React Device

--- src/App.js
+++ src/App.js
@@ -1,63 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import foto from '../src/images/Group 1.png';
+import styled from 'styled-components';
+//import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+
 
 function App() {
   return (
-    <Form>
-    <Row className="mb-3">
-      <Form.Group as={Col} controlId="formGridEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+    <>
+   <Row className='w-75 m-auto bg-success'>
+     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
+     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
+     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
+     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
+   </Row>
 
-      <Form.Group as={Col} controlId="formGridPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-    </Row>
+   <Row className='w-75 m-auto bg-success'>
+     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
+     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
+     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
+     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
+   </Row>
+   
 
-    <Form.Group className="mb-3" controlId="formGridAddress1">
-      <Form.Label>Address</Form.Label>
-      <Form.Control placeholder="1234 Main St" />
-    </Form.Group>
-
-    <Form.Group className="mb-3" controlId="formGridAddress2">
-      <Form.Label>Address 2</Form.Label>
-      <Form.Control placeholder="Apartment, studio, or floor" />
-    </Form.Group>
-
-    <Row className="mb-3">
-      <Form.Group as={Col} controlId="formGridCity">
-        <Form.Label>City</Form.Label>
-        <Form.Control />
-      </Form.Group>
-
-      <Form.Group as={Col} controlId="formGridState">
-        <Form.Label>State</Form.Label>
-        <Form.Select defaultValue="Choose...">
-          <option>Choose...</option>
-          <option>...</option>
-        </Form.Select>
-      </Form.Group>
-
-      <Form.Group as={Col} controlId="formGridZip">
-        <Form.Label>Zip</Form.Label>
-        <Form.Control />
-      </Form.Group>
-    </Row>
-
-    <Form.Group className="mb-3" id="formGridCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
+   </>
+   
+   
   );
 }
 
