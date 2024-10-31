b46122f58f3b798ad319fa6c334795f96c779c71
Julio B <juliobertholucciferraz@gmail.com>
2024-10-31 11:53:08 -0300

    Iniciando Projeto

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import foto from '../src/images/Group 1.png';
 import styled from 'styled-components';
 //import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 
 function App() {
   return (
-    <>
-   <Row className='w-75 m-auto bg-success'>
-     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
-     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
-     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
-     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
-   </Row>
-
-   <Row className='w-75 m-auto bg-success'>
-     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
-     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
-     <Col md="3" sm="6" className='bg-danger'>CONTENT</Col>
-     <Col md="3" sm="6" className='bg-primary'>CONTENT</Col>
-   </Row>
-   
-
-   </>
-   
+    
+   <></>
    
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body{
-    background-color:#fff;
+    background-color:#000;
   }
 `
 
