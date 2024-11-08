f00f96b897534b45dab517689d6e9fc680aaf3fc
Julio B <juliobertholucciferraz@gmail.com>
2024-11-08 16:16:36 -0300

    create section 3

--- src/components/TextSection2/index.js
+++ src/components/TextSection2/index.js
@@ -4,23 +4,15 @@ import styled from 'styled-components';
 const Text = styled.p`
     color:#fff;
     font-size:22px;
-<<<<<<< HEAD
     text-align:center;
     @media (max-width: 1000px) {
         background-size:auto 100%;
         text-align:justify;
     }
 `
-
-function TextSection2(){
-    return(
-        <Text>"Iniciei em tecnologia pela ETEC Dr. Carolino da Motta e Silva, aos 15 anos, aprendendo lógica de programação, desenvolvimento web e design por 3 anos consecutivos. Avançando para a faculdade em 2022, ingressei na PUC Minas no curso de Ciência da Computação permanecendo por 1 ano, realizando projeto de iniciação científica. Durante o período de 2022 a 2024 criei agências de marketing, trabalhei como autônomo em desenvolvimento web e marketing. Hoje conto com mais de 5 anos de experiência e com 21 anos de idade..."</Text>
-=======
-`
 function TextSection2(){
     return(
         <Text className='text-center'>"Iniciei em tecnologia pela ETEC Dr. Carolino da Motta e Silva, aos 15 anos, aprendendo lógica de programação, desenvolvimento web e design por 3 anos consecutivos. Avançando para a faculdade em 2022, ingressei na PUC Minas no curso de Ciência da Computação permanecendo por 1 ano, realizando projeto de iniciação científica. Durante o período de 2022 a 2024 criei agências de makreting, trabalhei como autônomo em desenvolvimento web e marketing. Hoje conto com mais de 5 anos de experiência e com 21 anos de idade..."</Text>
->>>>>>> 9404e9d081155ed8bac016f7cd4cc027aa5e3ba3
     );
 }
 export default TextSection2;
