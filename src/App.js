9ca7b899ea78c200fc405adce1e2bd825a497980
Julio B <juliobertholucciferraz@gmail.com>
2024-11-13 20:56:05 -0300

    do componentization section 5

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Section1 from './components/Section 1';
 import Section2 from './components/Section 2';
 import Section3 from './components/Section 3';
 import Section4 from './components/Section 4';
+import MyCarousel from './components/Section 5';
 import styled from 'styled-components';
 //import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 const AppContainer = styled.div`
@@ -19,6 +20,7 @@ function App() {
       <Section2/>
       <Section3/>
       <Section4/>
+      <MyCarousel/>
     </AppContainer>
   );
 }
--- src/components/Section 4/index.js
+++ src/components/Section 4/index.js
@@ -6,19 +6,21 @@ const Section = styled.section`
     height:100vh;
     background-color:#000;
     background-position: bottom;
-    padding:0% 5% 0% 5%;
+    //padding:0% 5% 0% 5%;
     display:flex;
     justify-content:center;
     align-items:center;
+    @media (max-width: 1000px) {
+        height:50vh;
+    }
 `
 const Video = styled.iframe`
-    width:75%;
+    width:100%;
     height:75%;
-    border-radius:30px;
-    box-shadow: 0 0 10px rgba(50, 129, 255, 0.75);
+    box-shadow: 0 0 10px rgba(50, 129, 255, 1);
     @media (max-width: 1000px) {
         width:100%;
-        height:50%;
+        height:90%;
     }
 `
 function Section4(){
